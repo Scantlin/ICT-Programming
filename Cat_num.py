fba0e1c216fedd52d5f6c3b47f555badcee34f5e
Scantlin <your.email@example.com>
2025-07-01 18:04:44 +0000

    Update

--- Cat_num.py
+++ Cat_num.py
@@ -1,60 +1,78 @@
-from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
-import numpy as np
-import pandas as pd
+from sklearn.model_selection import train_test_split #To split the data
+from sklearn.ensemble import RandomForestRegressor #To create a model using random forest
+from sklearn.metrics import r2_score, mean_absolute_error #To see the accuracy
+from sklearn.preprocessing import OneHotEncoder #To convert categorical data into numerical
+from sklearn.compose import ColumnTransformer #Upon convertion it is necessary to make it array
+import numpy as np 
+import pandas as pd #To access the data frame
+from scipy import stats
 
-def main():
-    data = {
-    'Size': [5, 7, 10, 12, 15, 8, 10, 20, 12, 18, 6, 9, 11, 14, 16, 4, 7, 13, 17, 19, 5, 8, 10, 15, 20, 10, 12, 14, 16, 18],
-    'Colors': ['red', 'Blue', 'Yellow', 'Green', 'Violet', 'red', 'Blue', 'Yellow', 'Green', 'Violet', 
-              'red', 'Blue', 'Yellow', 'Green', 'Violet', 'red', 'Blue', 'Yellow', 'Green', 'Violet',
-              'red', 'Blue', 'Yellow', 'Green', 'Violet', 'red', 'Blue', 'Yellow', 'Green', 'Violet'],
-    'Price': [120, 220, 320, 420, 500, 130, 230, 350, 450, 510, 
-              110, 210, 310, 410, 490, 100, 200, 300, 400, 480,
-              125, 225, 325, 425, 505, 135, 235, 355, 455, 515]
-}
+class Main:
+    def main(self):
+        try:
+            data = {
+                'Size': [5, 7, 10, 12, 15, 8, 10, 20, 12, 18, 6, 9, 11, 14, 16, 4, 7, 13, 17, 19, 5, 8, 10, 15, 20, 10, 12, 14, 16, 18],
+                'Colors': ['red', 'Blue', 'Yellow', 'Green', 'Violet', 'red', 'Blue', 'Yellow', 'Green', 'Violet', 
+                            'red', 'Blue', 'Yellow', 'Green', 'Violet', 'red', 'Blue', 'Yellow', 'Green', 'Violet',
+                            'red', 'Blue', 'Yellow', 'Green', 'Violet', 'red', 'Blue', 'Yellow', 'Green', 'Violet'],
+                'Gender': ['M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M', 'F', 'M',
+                            'F', 'M', 'F', 'M', 'M', 'M'],
+                'Price': [120, 220, 320, 420, 500, 130, 230, 350, 450, 510, 
+                        110, 210, 310, 410, 490, 100, 200, 300, 400, 480,
+                        125, 225, 325, 425, 505, 135, 235, 355, 455, 515]
+                }
 
-    dataframe = pd.DataFrame(data)
+            #Since the dataset is in dict type, make it a dataframe using the pandas
+            dataframe = pd.DataFrame(data)
+            
+            #Seperate the Independent Variable into the Dependent Var
+            Independent_var = dataframe.iloc[:, :-1]
+            Dependent_var = dataframe.iloc[:, -1]
 
-    X = dataframe[['Size', 'Colors']]
-    y = dataframe['Price']
+            '''
+            slope, intercept, r, p, std_error = stats.linregress(Independent_var, Dependent_var)
 
-    #Split Data
-    X_Train, X_Test, y_Train, y_Test = train_test_split(X, y, test_size=0.2, random_state=42)
+            info = {
+                "slope": slope,
+                "intercept": intercept,
+                "r": r,
+                "std_error": std_error
+            }
 
-    #Variable = ColumnTransformer(transformers=[('new name', OneHotEncoder(drop='first', handle_unknown='ignore'), ['name of the column that has category'])], remainder='passthrough')
-    preprocessing = ColumnTransformer(
-        transformers=[('colors', OneHotEncoder(drop='first', handle_unknown='ignore'), ['Colors'])], 
-        remainder='passthrough')
+            print(info)'''
 
-    X_Train_encoded = preprocessing.fit_transform(X_Train)
-    X_Test_encoded = preprocessing.transform(X_Test)
+            #Split the dataframe into Train and Test
+            X_train, X_test, y_train, y_test = train_test_split(Independent_var, Dependent_var, test_size=0.2, random_state=42)
 
-    model = LinearRegression().fit(X_Train_encoded, y_Train)
+            #Transformed the categorical Data into Numerical Data but in a Binary way since the data are nominal
+            Categorical_var = dataframe.select_dtypes(include=['object']).columns.tolist()
+            encoding = ColumnTransformer(transformers=[('cat', OneHotEncoder(sparse_output=False), Categorical_var)], remainder='passthrough', verbose_feature_names_out=False)
 
-    y_pred_train = model.predict(X_Train_encoded)
-    y_pred_test = model.predict(X_Test_encoded)
+            X_train_encoded = encoding.fit_transform(X_train)
+            X_test_encoded = encoding.transform(X_test)
 
-    Train_MSE = mean_squared_error(y_Train, y_pred_train)
-    Test_MSE = mean_squared_error(y_Test, y_pred_test)
+            #Creating a model
+            Model = RandomForestRegressor().fit(X_train_encoded, y_train)
+            y_pred = Model.predict(X_test_encoded)
 
-    Train_r2 = r2_score(y_Train, y_pred_train)
-    Test_r2 = r2_score(y_Test, y_pred_test)
+            error = abs(y_test - y_pred)
 
-    test_results = pd.DataFrame({
-        'Actual': y_Test,
-        'Predicted': y_pred_test,
-        'Error': y_Test - y_pred_test
-    })
+            Analysis = {
+                "Y prediction": y_pred.tolist(),
+                "Y test": y_test.tolist(),
+                'error': round(error, 2).tolist(),
+                'Absolute Error': sum(error)/len(error)
+            }
+            print(Analysis)
+            print(mean_absolute_error(y_test, y_pred))
 
-    print(test_results)
+        except Exception as e:
+            print(e)
 
-    print(f'TRAIN AND TEST MAE {mean_absolute_error(y_Train, y_pred_train)} {mean_absolute_error(y_Test, y_pred_test)}')
-    print(f'TRAIN MSE: {Train_MSE:.2f} TRAIN R2: {Train_r2:.2f}')
-    print(f'TEST MSE: {Test_MSE:.2f} TEST R2: {Test_r2:.2f}')
+        finally:
+            print('Thank you for using the program')
+
 
 if __name__ == '__main__':
-    main()
+    program = Main()
+    access = program.main()
