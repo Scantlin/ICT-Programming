078f8fe1d1af8b3bf972446f3b82cffe3a4b5177
Scantlin <your.email@example.com>
2025-09-21 11:34:05 +0000

    update

--- java_codes/switch_statement.java
+++ java_codes/switch_statement.java
@@ -1,19 +1,38 @@
+import java.util.Scanner;   
 public class switch_statement{
     public static void main(String[] args){
-        char grade = 'B';
+        Scanner Scan = new Scanner(System.in); //Object for Scanner
 
-        switch(grade){
-            case 'A':
-                System.out.println(grade + ": " + "outstanding");
+        System.out.println("Enter the numbers: ");
+        int First_num = Scan.nextInt();
+        int Second_num = Scan.nextInt();
+
+        System.out.println("Enter your Operation");
+        String operation = Scan.next();
+
+        switch(operation){
+            case "sum":
+                System.out.println("Addition: " + (First_num + Second_num));
                 break;
-            case 'B':
-                System.out.println("Excellence");
+            case "Subtract":
+                System.out.println("Differences: " + (First_num - Second_num));
                 break;
-            case 'C':
-                System.out.println("Satisfactory");
+            case "division":
+                try{
+                    System.out.println("Division: " + (First_num / Second_num));
+                }
+                catch(Exception e){
+                    System.out.println("There is an error");
+                }
+                break;
+            case "multiplication":
+                System.out.println("Multiply: " + (First_num * Second_num));
                 break;
             default:
-                System.out.println("Invalid Grade");
+                System.out.println("Invalid Operation");
         }
+
+        //Close the Scanner
+        Scan.close();
     }
 }
