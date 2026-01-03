a2ab44a89445a0819612a845484dcdf772ff194f
Scantlin <scantlincayson370@gmail.com>
2026-01-04 02:09:08 +0800

    NLP_practice

--- playground.py
+++ playground.py
@@ -1,5 +1,33 @@
-print("Bag of words")
 
+class bag_words:
+    def __init__(self, num_sentence):
+        self.num_sentence = num_sentence
+    
+    def storage(self):
+        self.words = []
+
+    def sentence(self):
+        self.storage() #Call the method
+        for i in range(self.num_sentence):
+            self.sentence = input(f"{i+1}: ").split(" ")
+            self.sentence = [word.capitalize() for word in self.sentence]
+
+            for self.word in self.sentence:
+                if self.word in self.words:
+                    pass
+                else:
+                    self.words.append(self.word)
+    def call(self):
+        self.sentence()
+        self.words.sort()
+        return self.words
+
+if __name__ == "__main__":
+    user_input_num = int(input("Enter the number of sentence: "))
+    start = bag_words(user_input_num).call()
+    print(start)
+
+'''
 num_sentence = int(input("Enter the number of sentences: "))
 words = []
 
@@ -12,4 +40,4 @@ for i in range(num_sentence):
         else:
             words.append(word)
 
-print(words)
+print(words)'''
