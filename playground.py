17671d9e9b9d55901e9b8e262685641da480f448
Scantlin <scantlincayson370@gmail.com>
2026-01-05 23:45:09 +0800

    NLP_playground

--- playground.py
+++ playground.py
@@ -4,13 +4,27 @@ class bag_words:
         self.num_sentence = num_sentence
     
     def storage(self):
+        self.sentence_bucket = []
         self.words = []
+
+    def vectors(self):
+        self.base_bucket = [[] for i in range(self.num_sentence)]
+        self.words.sort()
+
+        for i in range(len(self.base_bucket)):
+            for word in (self.words):
+                self.base_bucket[i].append(self.sentence_bucket[i].split(" ").count(word))
+                #print(word)
 
     def sentence(self):
         self.storage() #Call the method
+
         for i in range(self.num_sentence):
-            self.sentence = input(f"{i+1}: ").split(" ")
-            self.sentence = [word.capitalize() for word in self.sentence]
+            self.sentence = input(f"{i+1}: ").lower().split(" ")
+            #self.sentence_bucket.append(self.word_sample)
+            self.sentence = [word.lower() for word in self.sentence]
+            self.join_again_sentence = " ".join(self.sentence)
+            self.sentence_bucket.append(self.join_again_sentence)
 
             for self.word in self.sentence:
                 if self.word in self.words:
@@ -20,16 +34,29 @@ class bag_words:
                         self.words.append(self.word)
                     else:
                         pass
+        
+        self.vectors()
+
     def call(self):
         self.sentence()
         self.words.sort()
-        return self.words
-
+        return self.words, self.sentence_bucket, self.base_bucket
+        
 '''if __name__ == "__main__":
-    user_input_num = int(input("Enter the number of sentence: "))
-    start = bag_words(user_input_num).call()
-    print(start)'''
+    try:
+        user_input_num = int(input("Enter the number of sentence: "))
+        start = bag_words(user_input_num)
+        output, output2, vectors = start.call()
+        print(output)
+        print(output2)
+        print(vectors)
 
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        print("Thank you")'''
+    
 '''
 num_sentence = int(input("Enter the number of sentences: "))
 words = []
