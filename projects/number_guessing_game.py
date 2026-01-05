0813d8bddcfbdf6e95ace6fda91996d9dd718a02
Scantlin <scantlincayson370@gmail.com>
2026-01-06 00:12:55 +0800

    updateee

--- projects/number_guessing_game.py
+++ projects/number_guessing_game.py
@@ -6,7 +6,14 @@ class number_guessing_game:
         self.number = random.randint(0, 10)
     
     def play(self, guess):
-        print()
-
+        self.guess = guess
+        if self.guess == self.number:
+            print("You guessed it right!")
+        else:
+            print("not right")
+        
 if __name__ == "__main__":
     game = number_guessing_game()
+    while True:
+        guess_input = int(input("Enter your guess: "))
+        game.play(guess_input)
