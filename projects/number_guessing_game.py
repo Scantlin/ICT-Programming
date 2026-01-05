5b6ebf1034e11da075da60622b2322c034554a6a
Scantlin <scantlincayson370@gmail.com>
2026-01-05 23:58:06 +0800

    updatee

--- projects/number_guessing_game.py
+++ projects/number_guessing_game.py
@@ -2,7 +2,11 @@ import random
 
 class number_guessing_game:
     def __init__(self):
-        self.number = random.randint(0, 100)
+        print("Welcome to Guessing Game")
+        self.number = random.randint(0, 10)
+    
+    def play(self, guess):
+        print()
 
 if __name__ == "__main__":
     print(number_guessing_game().number)
