c5be3821df763b65aa1d519bf59c53422ef77b3c
Scantlin <scantlincayson370@gmail.com>
2026-01-07 02:58:43 +0800

    updateeee

--- projects/number_guessing_game.py
+++ projects/number_guessing_game.py
@@ -2,17 +2,23 @@ import random
 
 class number_guessing_game:
     def __init__(self):
-        print("Welcome to Guessing Game")
+        print("----- Welcome to Guessing Game -----")
         self.number = random.randint(0, 10)
+        self.conti = True
     
     def play(self, guess):
         self.guess = guess
         if self.guess == self.number:
             print("You guessed it right!")
-            return False
+            self.conti = False
+        elif self.guess > self.number and self.guess <= self.number:
+            print("lower your guess")
+        elif self.guess < self.number and self.guess > 0:
+            print("higher your guess")
         else:
-            print("not right")
-            return True
+            print("You input invalid number")
+
+        return self.conti
         
 if __name__ == "__main__":
     game = number_guessing_game()
